0b709f291ce30482685046403b0ca3f8fc57527d
Shivam Nath <shivamnath027@gmail.com>
2023-10-19 23:03:11 +0530

    Dsa

--- selection_sort_desc.cpp
+++ selection_sort_desc.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <limits.h>
 using namespace std;
 int main(){
     int n;
     cout<<"Enter the size: ";
     cin>>n;
-       
+    int arr[n];
+    cout<<"\nEnter Array Elements: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        int min = INT_MAX;
+        
+    }
 }
