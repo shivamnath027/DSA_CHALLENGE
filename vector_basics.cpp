fc6cc0fdae665241d66173c9026791d98589bf43
Shivam Nath <shivamnath027@gmail.com>
2023-10-24 16:58:39 +0530

    Dsa

--- vector_basics.cpp
+++ vector_basics.cpp
@@ -5,24 +5,24 @@ int main(){
     // DECLARATION:
     //1
     //WITHOUT DECLARING SIZE
-    vector<int> v;
+    // vector<int> v;
     //2
     //vector<int> v(sizeofvector);
-    vector<int> v(4);
+    // vector<int> v(4);
     //3
     //INITIALIZING ALL THE 4 ELEMENTS with value 2
-    vector<int> v(4,2);
+    // vector<int> v(4,2);
     //INSERTING VALUES------------------------------------------------------------.>
-    v.push_back(4);
-    v.push_back(8);
-    v.push_back(5);
+    // v.push_back(4);
+    // v.push_back(8);
+    // v.push_back(5);
 
     // The Size will be increased by multiplying by 2
     //for ex: if current size if 1, after you try to push elements it will
     // make the size 2, then it will make it 4, then 8 then 16 and so on
     
 
-    // vector<int> v = {2,4,6,8,10};
+    vector<int> v = {2,4,6,8,10};
     // ASK THE USER
     // cin>>n;
     // vector<int> v(n);
