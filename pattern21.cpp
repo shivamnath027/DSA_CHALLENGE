7d9c838f76d01e834a97766faefe81fbcc060c4b
Shivam Nath <shivamnath027@gmail.com>
2023-10-29 20:24:30 +0530

    Dsa

--- pattern21.cpp
+++ pattern21.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cout<<"N: ";
-    cin>>n;
-    int space=n-1;
-    for(int i=1;i<=n;i++){
-        for(int j=space;j>=1;j--){
-            cout<<" ";
+    cout << "N: ";
+    cin >> n;
+    int space = n - 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = space; j >= 1; j--)
+        {
+            cout << " ";
         }
-        for(int k=1;k<=i;k++){
-            cout<<"* ";
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
         space--;
     }
-    
     space = 0;
-    for(int i=n;i>=1;i--){
-        for(int j=1;j<=space;j++){
-            cout<<" ";
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
         }
-        for(int k=i;k>=1;k--){
-            cout<<"* ";
+        for (int k = i; k >= 1; k--)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
         space++;
     }
     return 0;
