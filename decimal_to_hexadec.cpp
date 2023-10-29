2ec36239d35f6f5cb0373fa9df33928c0511ba2b
Shivam Nath <shivamnath027@gmail.com>
2023-10-29 20:22:23 +0530

    Dsa

--- decimal_to_hexadec.cpp
+++ decimal_to_hexadec.cpp
@@ -1,35 +1,43 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main(){
+int main()
+{
 
-    int num;
-    cout<<"N: ";
-    cin>>num;
-    int rem=0;
-    string ans="";
-    while(num>0){
-        rem = num % 16;
-        switch(rem){
-            case 10: ans +='A';
-                    break;
-            case 11: ans+= 'B';
-                    break;
-            case 12: ans+= 'C';
-                    break;
-            case 13: ans+='D';
-                    break;
-            case 14: ans+='E';
-                    break;
-            case 15: ans+='F';
-                    break;
-            default:
-                ans = to_string(rem) + ans ;
-                break;
+        int num;
+        cout << "N: ";
+        cin >> num;
+        int rem = 0;
+        string ans = "";
+        while (num > 0)
+        {
+                rem = num % 16;
+                switch (rem)
+                {
+                case 10:
+                        ans += 'A';
+                        break;
+                case 11:
+                        ans += 'B';
+                        break;
+                case 12:
+                        ans += 'C';
+                        break;
+                case 13:
+                        ans += 'D';
+                        break;
+                case 14:
+                        ans += 'E';
+                        break;
+                case 15:
+                        ans += 'F';
+                        break;
+                default:
+                        ans = to_string(rem) + ans;
+                        break;
+                }
+                num = num / 16;
         }
-        num = num/16;
-    }
-    cout<<"\nHexaDecimal: "<<ans<<endl;
-    return 0;
-    
+        cout << "\nHexaDecimal: " << ans << endl;
+        return 0;
 }
