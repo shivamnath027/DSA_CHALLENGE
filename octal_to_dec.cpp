c4042311dc7a30d29697fac25319653c97ba9dcb
Shivam Nath <shivamnath027@gmail.com>
2023-11-06 19:04:57 +0530

    Dsa

--- octal_to_dec.cpp
+++ octal_to_dec.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int num;
-    cout<<"N: ";
-    cin>>num;
-    int rem=0,mul=1,ans=0;
-    while(num>0){
+    cout << "N: ";
+    cin >> num;
+    int rem = 0, mul = 1, ans = 0;
+    while (num > 0)
+    {
         rem = num % 10;
-        num = num/10;
-        ans = mul*rem+ans;
-        mul =mul*8;
+        num = num / 10;
+        ans = mul * rem + ans;
+        mul = mul * 8;
     }
-    cout<<"\nDecimal: "<<ans<<endl;
+    cout << "\nDecimal: " << ans << endl;
     return 0;
 }
