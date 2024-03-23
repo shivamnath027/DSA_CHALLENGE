6b60f2b64ab81f769420f621ee0c549a636002cc
Shivam Nath <shivamnath027@gmail.com>
2024-03-23 13:58:50 +0530

    Dsa

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+using namespace std;
 int main()
 {
     int a[5] = {5, 4, 1, 2, 3};
+    for (int i = 1; i < 5; i++)
+    {
+        for (int j = i; j > 0; j--)
+        {
+            if (a[j] < a[j - 1])
+            {
+                swap(a[j], a[j - 1]);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
     for (int i = 0; i < 5; i++)
     {
-        
+        cout << a[i] << endl;
     }
 }
