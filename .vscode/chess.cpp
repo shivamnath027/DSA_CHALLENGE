a4a35abcfc8d62f9b6efd94acef7e92706fb1a0d
Shivam Nath <shivamnath027@gmail.com>
2024-03-23 18:40:45 +0530

    Dsa

---
+++ .vscode/chess.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, m;
+    cout << "row: ";
+    cin >> m;
+    cout << "column: ";
+    cin >> n;
+    int count = 0;
+    int SE1 = m, SE2 = n, SW1 = m, SW2 = n, NW1 = m, NW2 = n, NE1 = m, NE2 = n;
+    while ((SE1 > 0 && SE1 <= 8) && (SE2 > 0 && SE2 <= 8))
+    {
+        count++;
+        SE2++;
+        SE1++;
+    }
+    while ((NE1 > 0 && NE1 <= 8) && (NE2 > 0 && NE2 <= 8))
+    {
+        count++;
+        NE2++;
+        NE1++;
+    }
+    while ((NW1 > 0 && NW1 <= 8) && (NW2 > 0 && NW2 <= 8))
+    {
+        count++;
+        NW2++;
+        NW1++;
+    }
+    while ((SE1 > 0 && SW1 <= 8) && (SE2 > 0 && SW2 <= 8))
+    {
+        count++;
+        SW1++;
+        SW2++;
+    }
+
+    cout << count << endl;
+}
