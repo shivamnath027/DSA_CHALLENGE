29d988b0f865ecb54a5307364a40ec4cc7d33c4b
Shivam Nath <shivamnath027@gmail.com>
2025-02-15 21:32:04 +0530

    DSA

--- Swapping_binary.java
+++ Swapping_binary.java
@@ -5,17 +5,18 @@ class Swapping_binary {
         Scanner ip = new Scanner(System.in);
         System.out.println("Enter number to convert: ");
         int n = ip.nextInt();
-        if(n==0){
+        if (n == 0) {
             System.out.println(0);
             return;
         }
-        int mul=1,rem=0,ans=0;
-        while(n>0){
-            ans = n % 2 ;
+        int mul = 1, rem = 0, ans = 0;
+        while (n > 0) {
+            ans = n % 2;
             n = n / 2;
-            rem = ans*mul + rem;
-            mul = mul*10;
+            rem = ans * mul + rem;
+            mul = mul * 10;
         }
         System.out.println(rem);
+        ip.close();
     }
 }
